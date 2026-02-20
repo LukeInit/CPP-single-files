40dd92b858030adc770c118af2617d0ce6d293ab
.Luke <36607381+LukeInit@users.noreply.github.com>
2026-02-20 23:15:00 +0000

    Made it better imo
    easier to add stuff and a good template, its easier to just change it in cheat engine

--- OpenProcessMemTemplate.cpp
+++ OpenProcessMemTemplate.cpp
@@ -1,48 +1,135 @@
-#include <windows.h>
 #include <iostream>
+#include <conio.h>
+#include <windows.h>
+#include <tlhelp32.h>
 
-const char* ApplicationName = "Calculator"; // must match window title exactly
+// =============================
+// Changeable (CONFIG)
+// =============================
 
-void GetPid(HWND hWnd);
+bool logging = true;
+
+constexpr const char* WinName = ""; //the window name
+
+// =============================
+// GLOBAL VARS
+// =============================
+
+
+uintptr_t address = ; // a cheat engine pointer similar to this
+
+
+
+// leave below alone
+float cashval;   // int normally but some games do float
+DWORD procid = 0;
+HANDLE handle = nullptr;
+DWORD BaseAddress;
+
+// =============================
+// Function Declarations
+// =============================
+void AddCash();
+
+
+
+// leave below alone
+void GetReqs();
+
+
+// =============================
+// main
+// =============================
 
 int main()
 {
-    HWND hWnd = FindWindowA(nullptr, ApplicationName);
+    GetReqs();
+    
 
-    GetPid(hWnd);
+    int menu;
+    std::cout << " enter 1 to add cash\n ";
+    std::cin >> menu;
 
-    int menumain;
-    std::cout << "1. Open Process Handle\n";
-    std::cin >> menumain;
+    for(int i; i != 1;){
+        system("cls");
+        if (menu == 1){AddCash();} 
 
-    if (menumain == 1 && hWnd != NULL)
-    {
-        DWORD pid;
-        GetWindowThreadProcessId(hWnd, &pid);
+        std::cout << " 1. Exit\n 2. Add more \n ";
+        std::cin >> i;
 
-        HANDLE handleToProcess = OpenProcess(PROCESS_ALL_ACCESS , FALSE, pid);
-
-        if (handleToProcess)
-            std::cout << "Process opened successfully\n";
-        else
-            std::cout << "Failed to open process\n";
     }
 
-    std::cin.get();
-    std::cin.get();
+    CloseHandle(handle);
+
     return 0;
 }
 
-void GetPid(HWND hWnd)
+
+// =============================
+// FUNCTIONS
+// place custom functions below
+// =============================
+
+        //example read and write
+
+void AddCash()
 {
-    if (hWnd == NULL)
-    {
-        std::cout << "Can't find window\n";
-    }
-    else
-    {
-        DWORD pid;
-        GetWindowThreadProcessId(hWnd, &pid);
-        std::cout << "Window found. PID: " << pid << "\n";
-    }
+    ReadProcessMemory(handle, reinterpret_cast<LPCVOID>(address),&cashval, sizeof(float), nullptr);
+    float CashAdd;
+    std::cout << " you have: " << cashval <<  " Input How cash to add\n ";
+    std::cin >> CashAdd;
+
+    float newcash = cashval + CashAdd;
+    WriteProcessMemory(handle,reinterpret_cast<LPVOID>(address),&newcash,sizeof(float), nullptr);
+
+    std::cout << "\n Cash added if its not changed interact with it\n\n";
+
 }
+
+
+
+// =============================
+// DO NOT EDIT BELOW 
+// All the import stuff below
+// Add new stuff above
+// =============================
+
+void GetReqs() 
+{
+    
+    HWND hwnd = FindWindowA(NULL, WinName);
+    if (hwnd){std::cout << " Window found: " << hwnd << "\n";}    
+  
+    GetWindowThreadProcessId(hwnd, &procid);
+    if (procid){std::cout << " Process found: " << procid <<"\n";}
+    
+    handle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, procid);
+    if (handle){std::cout << " handle: " << handle << "\n";}
+
+    
+    ReadProcessMemory(handle, reinterpret_cast<LPCVOID>(address),&cashval, sizeof(float), nullptr);
+    if (cashval){std::cout << " found: " << address << "\n Value : " << cashval << "\n";}
+    if(!logging){
+        system("cls");
+    }
+
+
+    if ((!hwnd || !procid || !handle || !cashval) && logging){ // cashval checks if its 0 turn of loggin if starting with 0
+        system("cls");
+        std::cout << " issue found \n"<<
+        " Handle: " << hwnd 
+        << "\n Proc id: " << procid 
+        << "\n Handle: " << handle
+        << "\n Pointer: " <<  procid
+        << "\n Pointer val: " << cashval 
+        << "\n";
+ 
+    return;
+    }
+
+    
+}
+
+
+
+
